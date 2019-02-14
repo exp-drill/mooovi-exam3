f7cbb4001e210d100a776b52ae1ed4d2716649b5
kohei-isoda <isodakohei0511@gmail.com>
2019-02-14 19:14:27 +0900

    errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < RankingController
 
   def show
     # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
-    @product = Product.find(params[:id]) # 問題3ではこのコードは消して新しくコードを書いてください
+    @product = Product.where(params[:id]) # 問題3ではこのコードは消して新しくコードを書いてください
   end
 
   def search
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,11 +8,10 @@ class ReviewsController < RankingController
 
   def create
     Review.create(create_params)
-    redirect_to controller: :products, action: :index
   end
 
   private
   def create_params
-    params.require(:review).permit(:rate, :review).merge(product_id: params[:product_id], user_id: current_user.id)
+    params.permit(:rate, :review).merge(product_id: params[:product_id], user_id: current_user.id)
   end
 end
